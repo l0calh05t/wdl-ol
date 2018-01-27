25b8eea4b15f7acd52f19774cb04055072519080
l0calh05t <l0calh05t@gmx.net>
2018-01-27 10:36:58 +0100

    merge cmake and iplugquake branches
    work in progress
    VST3 remains broken
    only IPlugEffect builds as VST2

--- IPlugExamples/IPlugEffect/IPlugEffect.cpp
+++ IPlugExamples/IPlugEffect/IPlugEffect.cpp
@@ -1,54 +1,114 @@
 #include "IPlugEffect.h"
 #include "IPlug_include_in_plug_src.h"
-#include "IControl.h"
-#include "resource.h"
+#include "IControls.h"
+#include "config.h"
 
-const int kNumPrograms = 1;
-
-enum EParams
+class IArcControl : public IKnobControl
 {
-  kGain = 0,
-  kNumParams
+public:
+  IArcControl(IPlugBaseGraphics& plug, IRECT rect, int paramIdx, float angle1 = -135.f, float angle2 = 135.f) : IKnobControl(plug, rect, paramIdx), mAngle1(angle1), mAngle2(angle2)
+  {
+    
+  }
+  
+  void Draw(IGraphics& graphics)
+  {
+    graphics.FillRect(COLOR_GRAY, mRECT.GetPadded(-2));
+    graphics.DrawRect(COLOR_BLACK, mRECT.GetPadded(-2));
+    double angle = mAngle1 + mValue * (mAngle2 - mAngle1);
+    graphics.FillArc(COLOR_BLUE, mRECT.MW(), mRECT.MH(), mRECT.W() * 0.44, mAngle1, angle);
+    graphics.DrawArc(COLOR_BLACK, mRECT.MW(), mRECT.MH(), mRECT.W() * 0.44, mAngle1, angle);
+    graphics.DrawRadialLine(COLOR_BLACK, mRECT.MW(), mRECT.MH(), angle, 0., mRECT.W() * 0.49);
+    graphics.FillCircle(COLOR_WHITE, mRECT.MW(), mRECT.MH(), mRECT.W() * 0.1);
+    graphics.DrawCircle(COLOR_BLACK, mRECT.MW(), mRECT.MH(), mRECT.W() * 0.1);
+    
+    angle = DegToRad(angle);
+    
+    float x1 = mRECT.MW() + cos(angle - 0.3) * mRECT.W() * 0.3;
+    float y1 = mRECT.MH() + sin(angle - 0.3) * mRECT.W() * 0.3;
+    float x2 = mRECT.MW() + cos(angle + 0.3) * mRECT.W() * 0.3;
+    float y2 = mRECT.MH() + sin(angle + 0.3) * mRECT.W() * 0.3;
+    float x3 = mRECT.MW() + cos(angle) * mRECT.W() * 0.44;
+    float y3 = mRECT.MH() + sin(angle) * mRECT.W() * 0.44;
+    
+    graphics.FillTriangle(COLOR_WHITE, x1, y1, x2, y2, x3, y3);
+    graphics.DrawTriangle(COLOR_BLACK, x1, y1, x2, y2, x3, y3);
+  }
+
+  double mAngle1;
+  double mAngle2;
 };
 
-enum ELayout
+class IPolyControl : public IKnobControl
 {
-  kWidth = GUI_WIDTH,
-  kHeight = GUI_HEIGHT,
-
-  kGainX = 100,
-  kGainY = 100,
-  kKnobFrames = 60
+public:
+  IPolyControl(IPlugBaseGraphics& plug, IRECT rect, int paramIdx) : IKnobControl(plug, rect, paramIdx)
+  {
+    
+  }
+  
+  void Draw(IGraphics& graphics)
+  {
+    float xarray[32];
+    float yarray[32];
+    int npoints = 3 + round(mValue * 29);
+    double angle = (-0.75 * PI) + mValue * (1.5 * PI);
+    double incr = (2 * PI) / npoints;
+    double cr = mValue * (mRECT.W() / 2.0);
+    
+    graphics.FillRoundRect(COLOR_GRAY, mRECT.GetPadded(-2), cr);
+    graphics.DrawRoundRect(COLOR_BLACK, mRECT.GetPadded(-2), cr);
+    
+    for (int i = 0; i < npoints; i++)
+    {
+      xarray[i] = mRECT.MW() + sin(angle + i * incr) * mRECT.W() * 0.45;
+      yarray[i] = mRECT.MH() + cos(angle + i * incr) * mRECT.W() * 0.45;
+    }
+    
+    graphics.FillConvexPolygon(COLOR_ORANGE, xarray, yarray, npoints);
+    graphics.DrawConvexPolygon(COLOR_BLACK, xarray, yarray, npoints);
+    
+  }
 };
+
 
 IPlugEffect::IPlugEffect(IPlugInstanceInfo instanceInfo)
-  :	IPLUG_CTOR(kNumParams, kNumPrograms, instanceInfo), mGain(1.)
+: IPLUG_CTOR(kNumParams, kNumPrograms, instanceInfo)
 {
-  TRACE;
+  TRACE; 
 
   //arguments are: name, defaultVal, minVal, maxVal, step, label
-  GetParam(kGain)->InitDouble("Gain", 50., 0., 100.0, 0.01, "%");
+  GetParam(kGain)->InitDouble("Gain", 0., 0., 100.0, 0.01, "%");
   GetParam(kGain)->SetShape(2.);
 
-  IGraphics* pGraphics = MakeGraphics(this, kWidth, kHeight);
-  pGraphics->AttachPanelBackground(&COLOR_RED);
+  //create user interface
+  IGraphics* pGraphics = MakeGraphics(*this, kWidth, kHeight, 30);
+  pGraphics->AttachPanelBackground(COLOR_RED);
+  
+  pGraphics->AttachControl(new IArcControl(*this, IRECT(30, 100, 130, 200), kGain));
+  pGraphics->AttachControl(new IPolyControl(*this, IRECT(150, 100, 250, 200), -1));
+//  pGraphics->AttachControl(new ITextControl(*this, IRECT(kTextX, kTextY, 290, kTextY+10), DEFAULT_TEXT, GetBuildInfoStr()));
 
-  IBitmap knob = pGraphics->LoadIBitmap(KNOB_ID, KNOB_FN, kKnobFrames);
-
-  pGraphics->AttachControl(new IKnobMultiControl(this, kGainX, kGainY, kGain, &knob));
+  WDL_String buildInfo;
+  GetBuildInfoStr(buildInfo);
+  printf("%s", buildInfo.Get());
 
   AttachGraphics(pGraphics);
+//  pGraphics->EnableLiveEdit(true);
+  PrintDebugInfo();
 
   //MakePreset("preset 1", ... );
-  MakeDefaultPreset((char *) "-", kNumPrograms);
+  MakeDefaultPreset("-", kNumPrograms);
 }
 
 IPlugEffect::~IPlugEffect() {}
 
-void IPlugEffect::ProcessDoubleReplacing(double** inputs, double** outputs, int nFrames)
+void IPlugEffect::ProcessBlock(double** inputs, double** outputs, int nFrames)
 {
-  // Mutex is already locked for us.
-
+  mParams_mutex.Enter();
+  const double gain = GetParam(kGain)->Value() / 100.;
+  mParams_mutex.Leave();
+  
   double* in1 = inputs[0];
   double* in2 = inputs[1];
   double* out1 = outputs[0];
@@ -56,28 +116,7 @@ void IPlugEffect::ProcessDoubleReplacing(double** inputs, double** outputs, int 
 
   for (int s = 0; s < nFrames; ++s, ++in1, ++in2, ++out1, ++out2)
   {
-    *out1 = *in1 * mGain;
-    *out2 = *in2 * mGain;
-  }
-}
-
-void IPlugEffect::Reset()
-{
-  TRACE;
-  IMutexLock lock(this);
-}
-
-void IPlugEffect::OnParamChange(int paramIdx)
-{
-  IMutexLock lock(this);
-
-  switch (paramIdx)
-  {
-    case kGain:
-      mGain = GetParam(kGain)->Value() / 100.;
-      break;
-
-    default:
-      break;
+    *out1 = *in1 * gain;
+    *out2 = *in2 * gain;
   }
 }
